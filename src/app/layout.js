682066b7b77ca859020a76e7432e72b833e02d3b
Federico del Corro <fede_delcorro@hotmail.com>
2024-10-15 14:02:27 -0300

    layout cuadricualdo

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,12 +23,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="forest" className="scroll-smooth">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:20px_20px]`}
       >
         <NavBar />
         {children}
 
         <Footer />
+        {/* <div class="relative h-full w-full bg-black">
+          <div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+          <div class="absolute z-0 left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div>
+        </div> */}
       </body>
     </html>
   );
